c8a04e0cb1716105b6d4c7f783448b95a0adb804
Leandro Gonnçalves <leandro.gwn@gmail.com>
2024-02-10 02:13:20 -0300

    feet: componente inicial e estilização.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { InicialComponent } from './component/pagina/inicial/inicial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    InicialComponent
   ],
   imports: [
     BrowserModule,

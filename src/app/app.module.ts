8d0a7012f83d01fb25c845738a46e98e93f6dc1d
Leandro Gonnçalves <leandro.gwn@gmail.com>
2024-02-10 18:38:53 -0300

    feet: Inclusão de footer e estilizações.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './component/sidebar/sidebar.component';
 import { InicialComponent } from './component/pagina/inicial/inicial.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    InicialComponent
+    InicialComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
